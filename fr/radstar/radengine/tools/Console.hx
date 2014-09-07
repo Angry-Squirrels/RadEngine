864f75b25964bf6b0e381fcfcd58f10af8877192
TBaudon <baudon.thomas@gmail.com>
2014-09-07 23:46:04 +0200

    command history. Better ergonomy of command line.

--- fr/radstar/radengine/tools/Console.hx
+++ fr/radstar/radengine/tools/Console.hx
@@ -24,6 +24,9 @@ class Console extends Sprite
 	var mInput : TextField;
 	
 	var mCommands : Map<String, Dynamic>;
+	
+	var mCommandHistoy : Array<String>;
+	var mHistoryPos : Int;
 
 	public function new(game : RadGame, engine : Engine) 
 	{
@@ -31,6 +34,8 @@ class Console extends Sprite
 		
 		mGame = game;
 		mEngine = engine;
+		mCommandHistoy = new Array<String>();
+		mHistoryPos = 0;
 		initInput();
 		
 		mCommands = new Map<String, Dynamic>();
@@ -46,42 +51,60 @@ class Console extends Sprite
 	
 	function onKeyDown(e:KeyboardEvent):Void 
 	{
-		if (e.keyCode == Keyboard.TAB && e.ctrlKey) {
+		if (e.keyCode == Keyboard.D && e.ctrlKey && e.altKey) {
+			
+			e.stopImmediatePropagation();
 			
 			mVisible = !mVisible;
 			
-			if (mVisible)
+			if (mVisible){
 				Lib.current.stage.addChild(this);
+				Lib.current.stage.focus = mInput;
+			}
 			else
 				Lib.current.stage.removeChild(this);
 		}else if (e.keyCode == Keyboard.ENTER && mVisible) {
 			e.stopImmediatePropagation();
 			
-			var command = mInput.text.substr(2);
+			var command = mInput.text;
 			var name = command.split(' ')[0];
-			var params : Array<Dynamic> = command.split(' ');
+			var params : Array<String> = command.split(' ');
 			params.shift();
+			
+			var paramToApply = new Array<Dynamic>();
 			
 			for (i in 0 ... params.length) {
 				if (params[i] == 'false')
-					params[i] = false;
+					paramToApply.push(false);
 				else if (params[i] == 'true')
-					params[i] = true;
+					paramToApply.push(true);
+				else
+					paramToApply.push(params[i]);
 			}
 			
 			if (mCommands[name] != null) {
 				try{
 					var o = mCommands[name];
-					Reflect.callMethod(o, Reflect.field(o, name), params);
-					mInput.text = '=>';
+					Reflect.callMethod(o, Reflect.field(o, name), paramToApply);
+					mCommandHistoy.push(command);
+					mHistoryPos = mCommandHistoy.length - 1;
 				}catch (e : Dynamic) {
 					trace(e);
 				}
+			}else {
+				trace("No such command");
 			}
+		}else if (e.keyCode == Keyboard.UP && mVisible) {
+			e.stopImmediatePropagation();
+			
+			if (mCommandHistoy.length > 0) { 
+				mHistoryPos--;
+				if (mHistoryPos < 0)
+					mHistoryPos = mCommandHistoy.length - 1;
+				mInput.text = mCommandHistoy[mHistoryPos];
+			}
+			
 		}
-		
-		if (mInput.text.length < 2 || (mInput.text.length == 2 && mInput.text != "=>"))
-			mInput.text = "=>";
 	}
 	
 	function initInput():Void 
@@ -90,7 +113,6 @@ class Console extends Sprite
 		mInput.defaultTextFormat = new TextFormat('arial', 14, 0x00cc00, true);
 		mInput.multiline = false;
 		mInput.type = TextFieldType.INPUT;
-		mInput.text = "=>";
 		mInput.width = Lib.current.stage.stageWidth;
 		mInput.height = 20;
 		mInput.background = true;
